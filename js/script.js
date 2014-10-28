b9f6723a3f9092c0ca7487aaa04e2790cb854041
mdixon47 <mdixon@rap.midco.net>
2014-10-27 20:24:56 -0600

    add jquery files

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	$(window).scroll(function(event){
+		var y = $(this).scrollTop();
+
+		if (y >= 300) {
+
+			$('#benefit-info').addClass('animated fadeInDown');
+			$('.scale-with-grid').addClass('animated fadeInDown');
+		}
+
+		if (y >= 1000) {
+
+			$('.pricebox').addClass('animated fadeInDown');
+			$('.pricing-info').addClass('animated fadeInDown');
+		}
+
+		if (y >= 1800) {
+
+			$('.test-info').addClass('animated fadeInDown');
+			$('.test_1').addClass('animated fadeInLeft');
+			$('.test_2').addClass('animated fadeInDown');
+			$('.test_3').addClass('animated fadeInRight');
+		}
+
+	});
+
+});
